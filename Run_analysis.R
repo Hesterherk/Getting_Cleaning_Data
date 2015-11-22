0af0af02c9eefb8e97d32025d48147d47a0e5f25
Hesterherk <hester.vanherk@gmail.com>
2015-11-22 21:58:59 +0100

    Create Run_analysis.R

---
+++ Run_analysis.R
@@ -0,0 +1,63 @@
+
+
+
+library(reshape2)
+
+# Load activity labels + features
+activityLabels <- read.table("~/activity_labels.txt")
+activityLabels[,2] <- as.character(activityLabels[,2])
+features <- read.table("~/features.txt")
+features[,2] <- as.character(features[,2])
+
+# Extract only the data on mean and standard deviation (features chosen)
+Chosen_features <- grep(".*mean.*|.*std.*", features[,2])
+Chosen_features_names <- features[Chosen_features,2]
+
+# Give better labels than the ones provided in the data set
+Chosen_features_names = gsub('-mean', 'Mean', Chosen_features_names)
+Chosen_features_names = gsub('-std', 'Std', Chosen_features_names)
+Chosen_features_names <- gsub('[-()]', '', Chosen_features_names)
+
+
+# Load the datasets for training
+train <- read.table("~/train/X_train.txt")[Chosen_features]
+trainAct <- read.table("~/train/Y_train.txt")
+trainSub <- read.table("~/train/subject_train.txt")
+
+## combine columnwise data Subjects(30 subjects), Activities(6 activities), train dataset
+train <- cbind(trainSub, trainAct, train)
+#str(train)
+
+# Load the datasets for testing
+test <- read.table("~/test/X_test.txt")[Chosen_features]
+testAct <- read.table("~/test/Y_test.txt")
+testSub <- read.table("~/test/subject_test.txt")
+# and combine them 
+test <- cbind(testSub, testAct, test)
+
+# merge datasets and add labels
+Total <- rbind(train, test)
+colnames(Total) <- c("subject", "activity", Chosen_features_names)
+
+# turn activities & subjects into factors
+Total$activity <- factor(Total$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+Total$subject <- as.factor(Total$subject)
+
+# melt: the melt function is on "The melt function takes data in wide format and stacks all 
+#  columns containing measures into a single column of data. To make use of the function we need to 
+# specify a data frame, the id variables (which will be left at their settings) and 
+# the measured variables (columns of data) to be stacked. The id variables identify the variabels that are not
+# stacked.
+
+Total_melt <- melt(Total, id = c("subject", "activity"))
+
+# dcast creates a dataset in which all observations of the respective variables are summarized. 
+# In this case the mean is calculated across each activity a subject has measures of. 
+
+Total_mean <- dcast(Total_melt, subject + activity ~ variable, mean)
+
+# writing the result of allData.mean to a .txt file.
+
+write.table(Total_mean, "~/Move_mean.txt", row.names = FALSE, quote = FALSE)
+
+
